e0457687c2566f90c234ba8c99f24fc35aa289b1
jesussantana <jesus@esmarketingdigital.com>
2021-07-09 08:23:26 +0200

    Update files

--- {{ cookiecutter.repo_name }}/scripts/sturges.py
+++ {{ cookiecutter.repo_name }}/scripts/sturges.py
@@ -1,8 +1,10 @@
 # Sturges rule 
+# ==============================================================================
 
 import numpy as np
 
 def createBins(df, column):
+    
     k = int(np.ceil(1+np.log2(len(df[f'{column}']))))
-    plot = plt.hist(df[f'{column}'], bins= k)
-    return plot
+    
+    return k
